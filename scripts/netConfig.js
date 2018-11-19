f4f5b29c78918d5b5de5848b2c7e12f8131f7b04
LiuYing <lyingFr@163.com>
2018-11-20 00:40:07 +0800

    '提交完成'

--- scripts/netConfig.js
+++ scripts/netConfig.js
@@ -1,3 +1,3 @@
 window.PUBLIC='http://106.14.183.207:8086';//念念
-// window.PUBLIC='http://106.14.183.207:8085';//test
+// window.PUBLIC='http:///106.14.183.207:8085';//test
 // const PUBLIC='http://192.168.1.2:8085';//小黑电脑
