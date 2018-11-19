083d98321d1534ab25b206b364fe6e463ea18a3f
LiuYing <lyingFr@163.com>
2018-11-20 00:22:22 +0800

    v1.4 2018/11/20(4)修改

--- scripts/netConfig.js
+++ scripts/netConfig.js
@@ -1,2 +1,3 @@
-window.PUBLIC='http://106.14.183.207:8085';
+// window.PUBLIC='http://106.14.183.207:8086';//念念test
+window.PUBLIC='http://106.14.183.207:8085';//test
 // const PUBLIC='http://192.168.1.2:8085';//小黑电脑
