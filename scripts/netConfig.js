7c31cd4fb059f1bb061259f0bf1d764762928ae2
LiuYing <lyingFr@163.com>
2018-11-21 20:33:13 +0800

    V1.5 2018-11-21(1) 上线版 配置不同店铺 test2

--- scripts/netConfig.js
+++ scripts/netConfig.js
@@ -1,4 +1,4 @@
 // window.PUBLIC='http://106.14.183.207:8086';//念念
 window.PUBLIC='http://106.14.183.207:8085';//test
-window.SHOPID='58cc37ff221b';//zhangsan
-// window.SHOPID='eff1e110b12e';//lisi
+// window.SHOPID='58cc37ff221b';//zhangsan
+window.SHOPID='eff1e110b12e';//lisi
